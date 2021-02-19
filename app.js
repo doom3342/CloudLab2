f0ca8e29638bb3db30e1d8aa85c585dc0de592c5
doom3342 <79313029+doom3342@users.noreply.github.com>
2021-02-19 18:32:00 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ app.set('views', './views');
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Заголовок', message: 'Облачные технологии'});
-    pc.innerHTML="<img src='https://cf.ppt-online.org/files/slide/r/rXpTua8IhKyfU4vk0QCnmFGExP9BJcqHzZd6NL/slide-6.jpg'>";
-    
-   
+    res.render('index', {title: 'Лабораторная', message: 'Облачные технологии'});
 });
 
 app.get('/page2', function (req, res) {
