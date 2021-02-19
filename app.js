d53aadf5b9214c9cc4c04fe0c4f850e3dc011dc6
doom3342 <79313029+doom3342@users.noreply.github.com>
2021-02-19 18:19:35 +0600

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ app.set('views', './views');
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Обучение коду JS', message: 'Лабораторная работа №2!'});
+    res.render('index', {title: 'Обучение коду JS', message: 'Облачные технологии'});
+    <img src="https://adolforamirez.es/wp-content/uploads/2018/01/facilitadores-inhibidores-800x600.jpg">
 });
 
 app.get('/page2', function (req, res) {
-    res.render('page2', {title: 'Здесь вторая страница', message: 'Это - вторая страница!'});
+    res.render('page2', {title: 'Здесь вторая страница', message: 'Вторая страница'});
+    <p>
+  <a href="/"
+     title="Лучшее место для поиска дополнительной информации
 });
 
 app.listen(port, function() {
